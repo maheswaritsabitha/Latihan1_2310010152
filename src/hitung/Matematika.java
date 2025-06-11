6d353ac4e1f6694c0a6169015be7f8e62f816233
maheswaritsabitha <maheswaritsabitha@gmail.com>
2025-06-11 12:09:15 +0800

    Menyiapkan proyek Latihan 1

--- src/hitung/Matematika.java
+++ src/hitung/Matematika.java
@@ -1,13 +1,15 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package hitung;
 
-/**
- *
- * @author acer
- */
 public class Matematika {
-    
+   private double bil1,  bil2;
+   
+   public Matematika(double bil1, double bil2){
+       this.bil1 = bil1;
+       this.bil2 = bil2;
+   }
+   
+   
+    public double setPenjumlahan(){
+       return bil1 + bil2; 
+   }
 }
